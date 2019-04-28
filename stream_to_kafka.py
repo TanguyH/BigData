3b44d316a3d42f6cf84d2bd9c273a6ca8570fe0a
TanguyH <tanguyh@icloud.com>
2019-04-28 23:56:55 +0200

    update : streams to Kafka
    Every second Producer streams all readings taken within that second. Information is routed to the topics.

--- stream_to_kafka.py
+++ stream_to_kafka.py
@@ -1,21 +1,23 @@
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
-from datetime import datetime
+from datetime import *
+import time
 
 DATA_LOCATION = "data"
 SORTED_FILE = "sorted_data.txt"
 LOCATION_FILE = "space_location.txt"
+SLEEP = 0.5
 
 # containers
 date_times = []
 location_ids = []
 sensor_types = []
-measurements = []
-voltages = []
+readings = []
 municipalities = []
 
 # picked random time in scope
-current_date = datetime.strptime("2017-03-15 00:00:00.001", "%Y-%m-%d %H:%M:%S.%f")
+#current_date = datetime.strptime("2017-03-15 00:00:00.001", "%Y-%m-%d %H:%M:%S.%f")
+current_date = datetime.strptime("2017-03-05 00:59:16.803", "%Y-%m-%d %H:%M:%S.%f")
 
 # define data destination
 data_file = "{}/{}".format(DATA_LOCATION, SORTED_FILE)
@@ -45,7 +47,9 @@ for sensor_reading in sorted_info.readlines():
 
     # store information
     location_ids.append(int(location_id))
-    #sensor_types.append(int(sensor_type))
+    sensor_types.append(int(sensor_type))
+    readings.append(sensor_reading)
+
     #if sensor_type != "3":
     #    measurements.append(float(measurement))
     #else:
@@ -64,10 +68,56 @@ while not reached_current_date:
     else:
         curr_start += 1
 
-print("LOG: old data identified")
+print("LOG: past data identified")
 
-# sent all previous information to Kafka
-for i in range(curr_start):
-    loc_id = location_ids[i]
-    loc_municipality = municipalities[loc_id - 1]
-    print("{} -> {}".format(loc_id, loc_municipality))
+second = timedelta(seconds=1)
+
+try:
+    producer = KafkaProducer(bootstrap_servers = ['localhost:9092'])
+    all_processed = False
+
+    while not all_processed:
+        # sent all previous information to Kafka
+        for i in range(curr_start):
+            loc_id = location_ids[i]
+            loc_municipality = municipalities[loc_id - 1]
+            loc_sensor_type = sensor_types[i]
+
+            topic = "{}-{}".format(loc_municipality, loc_sensor_type)
+            producer.send(topic, readings[i].encode())
+
+        print("LOG: sent 1s interval data")
+
+        # remove sent elements from arrays
+        if not all_processed:
+            date_times = date_times[curr_start:]
+            location_ids = location_ids[curr_start:]
+            sensor_types = sensor_types[curr_start:]
+            readings = readings[curr_start:]
+            print("LOG: next second data identified")
+
+        # prepare new batch to send every second
+        current_date = current_date + second
+
+        # find elements of the next second
+        curr_start = 0
+        reached_current_date = False
+        while not reached_current_date and not all_processed:
+            explored_date = date_times[curr_start]
+
+            if explored_date > current_date:
+                reached_current_date = True
+                print("LOG: found new interval")
+            else:
+                curr_start += 1
+
+                # all data processed
+                if curr_start == len(date_times):
+                    all_processed = True
+                    print("LOG: all data processed")
+        time.sleep(SLEEP)
+
+except KeyboardInterrupt:
+     print("Terminating ...")
+finally:
+    producer.close()
